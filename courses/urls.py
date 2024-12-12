511d9e58934bd7a177d88b0c2620eb2317e3ee5c
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-12-12 13:23:47 +0000

    Course Admin - Editing course

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.all_courses, name='courses'),
     path('<int:course_id>/', views.course_detail, name='course_detail'),
     path('add/', views.add_course, name='add_course'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
-
