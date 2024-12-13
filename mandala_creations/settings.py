86cffd8c35266e42e495eb10c249f9bb5fe2c7ba
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-12-13 11:47:58 +0000

    Chores

--- mandala_creations/settings.py
+++ mandala_creations/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '+ja3&y!&x#4u)$ub874^0!-x*x!k--g+*1@j7-=h0i&4^ctcvu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-meghanarajv-onlinemanda-7anr4ejzwng.ws.codeinstitute-ide.net','online-manadala-creations.herokuapp.com', 'localhost']
 
