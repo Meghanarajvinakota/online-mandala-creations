58089a82a4b79e5d4823ee4f1c51e790578b0f8f
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-12-13 11:29:11 +0000

    Added secret key to env file

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 
 os.environ["DATABASE_URL"] = "postgresql://neondb_owner:wYcfyj3ioR8x@ep-gentle-rice-a2cg1u6v.eu-central-1.aws.neon.tech/snub_lunar_wing_13087"
+os.environ["SECRET_KEY"] = "+ja3&y!&x#4u)$ub874^0!-x*x!k--g+*1@j7-=h0i&4^ctcvu"
