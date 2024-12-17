b2414f718ee946eb76db6228ca88bb384b7838cb
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-12-17 11:29:20 +0000

    Upgraded the python version

--- basket/tests.py
+++ basket/tests.py
@@ -1,3 +1,164 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+from courses.models import Course
 
-# Create your tests here.
+
+# ------------ VIEWS TESTING ------------
+
+
+class BasketViewTestCase(TestCase):
+    """
+    Test case for testing basket views.
+    """
+
+    def setUp(self):
+        """
+        Makes two sample users, one with superuser status and one without.
+        Next, makes a sample course object
+        """
+
+        username1 = "testUser"
+        email1 = "testuser@test.com"
+        password = "default123"
+        first_name = "Test"
+        last_name = "User"
+
+        username2 = "testUserStaff"
+        email2 = "testuser2@test.com"
+        password = "default123"
+        first_name = "Test"
+        last_name = "User"
+
+        user1 = User.objects.create(
+            username=username1,
+            email=email1,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+            )
+
+        user2 = User.objects.create(
+            username=username2,
+            email=email2,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+            )
+
+        user2.is_staff = True
+        user2.is_superuser = True
+
+        user1.save()
+        user2.save()
+
+        name = "test_course"
+        description = "test_course_description"
+        price = '999.99'
+        date_created = timezone.now()
+        course1 = Course.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            date_created=date_created
+            )
+
+        course1.save()
+
+    def test_basket_render_context(self):
+        """
+        Tests that the basket page is rendered properly
+        and the correct context is passed in
+        """
+
+        response = self.client.get('/basket/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'basket/basket.html', 'base.html'
+            )
+
+    def test_basket_add(self):
+        """
+        Adds the test course to the basket, tests whether it has been
+        added successfully. Then attempts to add a duplicate item to
+        the basket, checks that it is not successful.
+        """
+
+        # Get the most recently created course (test course)
+        course1 = Course.objects.latest('pk')
+
+        # Try to add the test course to the basket
+        response = self.client.post(('/basket/add/1/'), {
+            'course_id': 1,
+            'quantity': 1,
+            'redirect_url': '/',
+        }, follow=True)
+
+        # Check that the basket action executes successfully
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the test item (id = 1) was added to the basket
+        session = self.client.session
+        self.assertEqual(session['basket'], {'1': 1})
+
+        # Try to add the test course to the basket again
+        response = self.client.post(('/basket/add/1/'), {
+            'course_id': 1,
+            'quantity': 1,
+            'redirect_url': '/',
+        })
+
+        # Check that we are redirected
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the exception is raised
+        self.assertRaises(
+            Exception, msg=f'{course1.name} is already in your basket!'
+            )
+
+        # Check that a duplicate item was not added to the basket
+        self.assertEqual(session['basket'], {'1': 1})
+
+    def test_basket_remove(self):
+        """
+        Adds the test course to the basket, checks that it was added.
+        Then deletes the course from the basket, checks that the
+        basket is empty. Next, attempts to delete a non-existant item
+        from the basket, checks that it is unsuccessful.
+        """
+
+        # Try to add the test course to the basket
+        response = self.client.post(('/basket/add/1/'), {
+            'course_id': 1,
+            'quantity': 1,
+            'redirect_url': '/',
+        }, follow=True)
+
+        # Check that the basket add action executes successfully
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the test item (id = 1) was added to the basket
+        session = self.client.session
+        self.assertEqual(session['basket'], {'1': 1})
+
+        # Try to remove the test course from the basket
+        response = self.client.post('/basket/remove/1/', follow=True)
+
+        # Check that the basket remove action executes successfully
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the basket is empty, meaning deletion was successful
+        session = self.client.session
+        self.assertEqual(session['basket'], {})
+
+        # Try to remove the test course from the basket
+        # while the basket is empty
+        response = self.client.post('/basket/remove/1/')
+
+        # Check that we are redirected
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the error message is raised
+        self.assertRaises(
+            Exception, msg="Oops, that didn't work, please try again."
+            )
--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,359 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Category, Course
+from django.utils import timezone
 
-# Create your tests here.
+
+# ------------ MODEL TESTING ------------
+
+class TestCategory(TestCase):
+    """Tests the Category model in the courses app."""
+
+    def setUp(self):
+        """
+        Makes a sample Category object
+        """
+
+        name = "test_category"
+        display_name = "test_category_display"
+        category1 = Category.objects.create(
+            name=name,
+            display_name=display_name
+            )
+
+        category1.save()
+
+    def test_str(self):
+        """Tests the string method on the category."""
+        # Retrieves the most recently created category and gets its string
+        category1 = Category.objects.latest('pk')
+        category_string = str(category1.name)
+
+        # Cofirms the category string is correct.
+        self.assertEqual((category_string), (category1.name))
+
+
+class TestCourse(TestCase):
+    """Tests the Course model in the courses app."""
+
+    def setUp(self):
+        """
+        Makes a sample Course object
+        """
+
+        name = "test_course"
+        description = "test_course_description"
+        price = '999.99'
+        date_created = timezone.now()
+        course1 = Course.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            date_created=date_created
+            )
+
+        course1.save()
+
+    def test_str(self):
+        """Tests the string method on the course."""
+        # Retrieves the most recently created course and gets its string
+        course1 = Course.objects.latest('pk')
+        course_string = str(course1.name)
+
+        # Cofirms the course string is correct.
+        self.assertEqual((course_string), (course1.name))
+
+
+# ------------ VIEWS TESTING ------------
+
+class CourseViewTestCase(TestCase):
+    """
+    Test case for testing course views.
+    """
+
+    def setUp(self):
+        """
+        Creates two sample users, one of whom has staff & superuser
+        privileges (testUserStaff) and another who does not (testUser).
+        Then, creates a sample course.
+        """
+
+        username1 = "testUser"
+        email1 = "testuser@test.com"
+        password = "default123"
+        first_name = "Test"
+        last_name = "User"
+
+        username2 = "testUserStaff"
+        email2 = "testuser2@test.com"
+        password = "default123"
+        first_name = "Test"
+        last_name = "User"
+
+        user1 = User.objects.create(
+            username=username1,
+            email=email1,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+            )
+
+        user2 = User.objects.create(
+            username=username2,
+            email=email2,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+            )
+
+        user2.is_staff = True
+        user2.is_superuser = True
+
+        user1.save()
+        user2.save()
+
+        name = "test_course"
+        description = "test_course_description"
+        price = '999.99'
+        date_created = timezone.now()
+        course1 = Course.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            date_created=date_created
+            )
+
+        course1.save()
+
+    def test_courses_render_context(self):
+        """
+        Tests that the courses page is rendered properly
+        """
+
+        response = self.client.get('/courses/courses/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'courses/courses.html', 'base.html'
+            )
+
+        # Checks whether the context item is passed in
+        self.assertTrue(response.context['courses'])
+
+    def test_edit_course_render_form(self):
+        """
+        Checks that the edit course page cannot be accessed by non-superusers.
+        Tests the url path by passing in the primary key of new test
+        course. Checks that the course detail page is rendered properly.
+        Checks that the course detail view matches the test course passed
+        into the url.
+        """
+
+        # Get the most recently created user (testUserStaff)
+        test_user_staff = User.objects.latest('date_joined')
+
+        # Get the second most recently created user (testUser)
+        test_user = User.objects.filter().order_by('-pk')[1]
+
+        # Check that the two users are correctly retrieved
+        self.assertEqual(test_user_staff.username, 'testUserStaff')
+        self.assertEqual(test_user.username, 'testUser')
+
+        # Tests the url path
+        course1 = Course.objects.latest('pk')
+
+        # Log in as testUser (no staff privileges)
+        self.client.force_login(test_user)
+
+        # Try to access edit course page (staff-only)
+        response = self.client.get(f'/courses/courses/edit/{course1.pk}/')
+
+        # Check that page access is not granted
+        self.assertNotEqual(response.status_code, 200)
+
+        # Check that we do not reach the edit course page
+        self.assertTemplateNotUsed('courses/edit_course.html')
+
+        # Log in as testUserStaff (has staff privileges)
+        self.client.force_login(test_user_staff)
+
+        # Try to access course details page (staff-only)
+        response = self.client.get(f'/courses/courses/edit/{course1.pk}/')
+
+        # Check that page access is granted
+        self.assertEqual(response.status_code, 200)
+
+        # Checks that the page renders correctly
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'courses/edit_course.html',
+            'base.html'
+            )
+
+        # Get the most recent course
+        # (should be test_course that was just created)
+        courseToUpdate = Course.objects.latest('date_created')
+
+        # update the course
+        response = self.client.post((
+            f'/courses/courses/edit/{courseToUpdate.pk}/'
+            ), {
+            'name': "test_course_updated",
+            'description': "test_course_description_updated",
+            'price': '9.99',
+            'date_created': timezone.now(),
+            })
+
+        # Get the most recent post
+        # (should be test_course_updated that was just updated)
+        newTestCourse = Course.objects.latest('date_created')
+
+        course_string_name = str(newTestCourse.name)
+        course_string_description = str(newTestCourse.description)
+        course_string_price = str(newTestCourse.price)
+
+        # Check that the post has been successfuly updated
+        self.assertEqual(
+            (course_string_name), ("test_course_updated")
+            )
+        self.assertEqual(
+            (course_string_description), ("test_course_description_updated")
+            )
+        self.assertEqual(
+            (course_string_price), ("9.99")
+            )
+
+    def test_add_course_render_form(self):
+        """
+        Checks that the add course page cannot be accessed by non-superusers.
+        Checks that the page is rendered properly.
+        Creates a new test course using the page form.
+        Tests that the course was created successfully.
+        """
+
+        # Get the most recently created user (testUserStaff)
+        test_user_staff = User.objects.latest('date_joined')
+
+        # Get the second most recently created user (testUser)
+        test_user = User.objects.filter().order_by('-pk')[1]
+
+        # Check that the two users are correctly retrieved
+        self.assertEqual(test_user_staff.username, 'testUserStaff')
+        self.assertEqual(test_user.username, 'testUser')
+
+        # Get the object's pk
+        course1 = Course.objects.latest('pk')
+
+        # Log in as testUser (no staff privileges)
+        self.client.force_login(test_user)
+
+        # Try to access add course page (staff-only)
+        response = self.client.get(f'/courses/courses/add/')
+
+        # Check that page access is not granted
+        self.assertNotEqual(response.status_code, 200)
+
+        # Check that we do not reach the add course page
+        self.assertTemplateNotUsed('courses/add_course.html')
+
+        # Log in as testUserStaff (has staff privileges)
+        self.client.force_login(test_user_staff)
+
+        # Try to access add course page (staff-only)
+        response = self.client.get(f'/courses/courses/add/')
+
+        # Check that page access is granted
+        self.assertEqual(response.status_code, 200)
+
+        # Checks that the page renders correctly
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'courses/add_course.html',
+            'base.html'
+            )
+
+        # Get the ID of the most recently created course object
+        originalCourse = Course.objects.latest('pk')
+        originalCourseId = originalCourse.pk
+
+        # Create a new test course using the page form
+        response = self.client.post(('/courses/courses/add/'), {
+            'name': 'test_course_created',
+            'description': 'test_course_description_created',
+            'price': '99.99',
+            'date_created': timezone.now()
+            })
+
+        # Get the most recent course object
+        # (should be test_course_created that was just created)
+        newTestCourse = Course.objects.latest('pk')
+        newTestCoursetId = newTestCourse.pk
+
+        # Check that the most recent course created is
+        # not the original course, meaning a new one
+        # was successfully created
+        self.assertNotEqual((originalCourseId), (newTestCourseId))
+
+        # Check that the most recently created object
+        # is our test course object
+        self.assertEqual(
+            'test_course_created', Course.objects.latest('pk').name
+            )
+
+    def test_delete_course_render_form(self):
+        """
+        Checks that courses cannot be deleted by non-superusers.
+        Deletes the test object.
+        Checks that the test object was successfully deleted.
+        """
+
+        # Get the most recently created user (testUserStaff)
+        test_user_staff = User.objects.latest('date_joined')
+
+        # Get the second most recently created user (testUser)
+        test_user = User.objects.filter().order_by('-pk')[1]
+
+        # Check that the two users are correctly retrieved
+        self.assertEqual(test_user_staff.username, 'testUserStaff')
+        self.assertEqual(test_user.username, 'testUser')
+
+        # Get the most recently created course object
+        courseToDelete = Course.objects.latest('pk')
+
+        # Get the to-be-deleted course object's ID
+        deletedCourseId = (courseToDelete.pk)
+
+        # Log in as testUser (not superuser)
+        self.client.force_login(test_user)
+
+        # Attempt to delete the course object using the delete view
+        response = self.client.post(
+            f'/courses/courses/delete/{courseToDelete.pk}/'
+            )
+
+        # Check that access is denied
+        self.assertNotEqual(response.status_code, 200)
+
+        # Log in as testUserStaff (superuser)
+        self.client.force_login(test_user_staff)
+
+        # Delete the course object using the delete view
+        response = self.client.post(
+            f'/courses/courses/delete/{courseToDelete.pk}/', follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+
+        # Check if there are any course objects in the database,
+        # if not, that means that the deletion was successful
+        if Course.objects.exists():
+            # If there are course objects remaining, we need to
+            # check that the one we tried to delete has been deleted.
+            # Get the ID of the last course object in the database
+            newLastCourse = Course.objects.latest('pk')
+            newLastCourseId = newLastCourse.pk
+
+            # Check if the last course object in the database is the
+            # same one that we tried to delete, if not, that
+            # means the deletion was successful
+            self.assertNotEqual(deletedCourseId, newLastCourseId)
