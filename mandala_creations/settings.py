08627ad20029b2256fa3d49b62334635b2ca285f
Meghana Vinakota <meghana.penugonda@gmail.com>
2024-12-18 17:26:13 +0000

    styled 404 page

--- mandala_creations/settings.py
+++ mandala_creations/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+ja3&y!&x#4u)$ub874^0!-x*x!k--g+*1@j7-=h0i&4^ctcvu'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
