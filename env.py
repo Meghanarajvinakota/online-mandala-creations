6dbe1d98a54d143606296cd094de1ecd404dc04a
Meghana Vinakota <meghana.penugonda@gmail.com>
2025-01-10 16:56:39 +0000

    Cached the env file

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgresql://neondb_owner:wYcfyj3ioR8x@ep-gentle-rice-a2cg1u6v.eu-central-1.aws.neon.tech/snub_lunar_wing_13087"
-os.environ["SECRET_KEY"] = "e3*zc32)9k4f&nyix_$krp6+c%c-r(l358ck)fzf8ve7ccauik"
-os.environ["DEVELOPMENT"] = "1"
